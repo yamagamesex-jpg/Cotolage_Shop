96f91dbd35509802571f53a45a129a50f890280b
SH4rlkr <yamagamesex@gmail.com>
2026-03-29 15:37:13 +0600

    Update script.js

--- script.js
+++ script.js
@@ -708,6 +708,16 @@ let products = [
     "video": null,
     "active": true,
     "category": "Чебуреки"
+  },
+  {
+      "id": "afs1fd1-d2ac-4c86-81f1-d7e5f8b1cd0d",
+      "name": "Утиная грудка",
+      "description": "3600 тг/кг",
+      "price": 3600,
+      "image": "photo_5352996059916997714_y.png",
+      "video": null,
+      "active": true,
+      "category": "Грудки"
   }
 ];
 let filteredProducts = [];
